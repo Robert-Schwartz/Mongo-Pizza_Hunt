98ef3ad45f6e5359a6386933b00ca342f51f8f18
Robert Schwartz <bschwartz23@gmail.com>
2021-07-07 15:59:31 -0700

    Create the Pizza Controller Methods Skeleton

---
+++ controllers/comment-controller.js
@@ -0,0 +1,20 @@
+// Import models
+const { Comment, Pizza } = require('../models');
+
+// Comment Controller Object
+const commentController = {
+    // ADD comment to pizza
+    addComment() {
+
+    },
+
+    // REMOVE comment
+    removeComment() {
+
+    }
+};
+
+// export controller
+module.export = commentController;
+
+
