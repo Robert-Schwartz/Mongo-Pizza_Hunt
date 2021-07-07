1e4c51e102788716ecda514de04dc645d5f3c0ca
Robert Schwartz <bschwartz23@gmail.com>
2021-07-07 15:52:07 -0700

    Add pizza count virtual property

--- models/Pizza.js
+++ models/Pizza.js
@@ -27,15 +27,32 @@ const PizzaSchema = new Schema({
     //association to the Comments model
     comments: [
         {
+            //Tell Mongoose to expect an ObjectId and to tell it that its data comes from the Comment model.
             type: Schema.Types.ObjectId,
             ref: 'Comment'
             // the ref property tells the Pizza model which document to search to find the right comments
         }
     ]
+},
+    {
+        // use toJSON to tell schema to use Virtuals
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+        //We set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+    }
+);
+
+// Virtual Property
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
+
 
 // export the Pizza model
 module.exports = Pizza;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
+// import models
 const Pizza = require('./Pizza');
+const Comment = require('./Comment');
 
-module.exports = { Pizza };
+//export models
+module.exports = { Pizza, Comment };
