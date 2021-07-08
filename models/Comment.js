d006fdbb78f440faa987c8c870b9b6eccd610ed5
Robert Schwartz <bschwartz23@gmail.com>
2021-07-08 15:17:33 -0700

    Create the Comment Reply Schema and add virtuals to count replies and comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,68 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-// Data
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+// Comment Reply Schema
+// ==================================================
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    //add toJSON to add getters
+    {
+        toJSON: {
+            getters: true
+        }
     }
+);
+
+
+// Comment Schema
+// ==================================================
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// Virtual Property
+// ====================================================
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
 });
 
 // create the Comment model using the CommentSchema
