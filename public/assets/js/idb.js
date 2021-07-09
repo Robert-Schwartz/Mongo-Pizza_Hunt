e070915c17cb2995c697c1f7385184db6e81493f
Robert Schwartz <bschwartz23@gmail.com>
2021-07-08 17:42:09 -0700

    Add the Object Store new_pizza in indexedDB

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -2,9 +2,36 @@
 // db will store the connected database object
 let db;
 
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+// establish a connection to IndexedDB databasecalled 'pizza_hunt' and set it to version  1
 // request variable will act as an event listener for the database
 /* the open() method takes 2 parameters.
 1. name of the database to create
 2. the version of the database */
 const request = indexedDB.open('pizza_hunt', 1);
+
+// this event will emit if the database version changes
+// (non-existant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+    // save a reference to the database
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`,
+    // set it to have an auto incrementing primary key of sorts
+    db.createObjectStore('new_pizza', { autoIncrement: true });
+};
+
+// upon a successful
+request.onsuccess = function (event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+        // we haven't created this yet, but we will soon, so let's comment it out for now
+        // uploadPizza();
+    }
+};
+
+request.onerror = function (event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
